9c609c12825276222139db58bfc671e57be7a97a
Nathan Barnes <npbarnes@alaska.edu>
2016-02-01 16:31:44 -0900

    Removes amplitude from lambda_i calculation.

--- inputs.f90
+++ inputs.f90
@@ -116,7 +116,7 @@ module inputs
                   
                   omega_p = q*b0_init/mion
                   
-                  lambda_i = (3e8/sqrt((nf_init*amp/1e9)*q*q/(8.85e-12*mion)))/1e3
+                  lambda_i = (3e8/sqrt((nf_init/1e9)*q*q/(8.85e-12*mion)))/1e3
                                     
                   dx= lambda_i*dx_frac
                   dy=lambda_i*dx_frac           !units in km
