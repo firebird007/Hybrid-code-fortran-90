ee2d1eff8191cc45115636f46bb01b46271a8faf
Nathan Barnes <npbarnes@alaska.edu>
2016-02-02 15:39:23 -0900

    Lower Ni_Max to its minimum possible value.

--- dimensions.f90
+++ dimensions.f90
@@ -4,7 +4,7 @@ module dimensions
 
       integer, parameter:: nx = 32, ny = 3, nz = 32
 
-      integer*4, parameter:: Ni_max = 5000000
+      integer*4, parameter:: Ni_max = 57600
       
 end module dimensions
       
